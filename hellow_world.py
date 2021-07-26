35ba239974dd550c583dd5da7519948311b9d85a
Niroshika <niroshika@Niroshika.local>
2021-07-26 14:05:05 +0530

    Before talking about branchesa

--- hellow_world.py
+++ hellow_world.py
@@ -1,2 +1,24 @@
-print("Hello World")
-print("Niroshika")
+Put speaker pic small and logo big on the main page and
+Once click on it put the speaker big and logo small(or other way around)
+
+Add a totally different image on top, like "React Day New York" Hot dog image
+
+Put speaker image verrryy big on a side in pop up
+
+Highlight Tracks
+
+Add speaker image in the agenda
+
+Having the ticket just right side of the page, Once you type your name will appear as a ticket on right.
+
+Do not Cut off the bio, just add everything speaker want
+
+Highlight SM sharing more
+
+Add a scratch (like Lottery Ticket) somewhere in the page
+
+Once you click on session on agenda all the information will display full width
+(Like speaker image/logo, bio, abstract, etc.)
+
+More mobile friendly
+
